d6fb579d1765bee03c156d8042ff959ccc67b675
Justin2528 <38654142+Justin2528@users.noreply.github.com>
2019-02-01 19:07:40 +0800

    Update bot.js

--- bot.js
+++ bot.js
@@ -19,11 +19,6 @@ fs.readdir("./commands/", (err, files) => {
     
     if(err) console.log(err);
 
-    let jsfile = files.filter(f => f.split(".").pop() === "js")
-    if(jsfile.length <= 0){
-        console.log("Couldn't find commands.")
-        return;
-    }
 
     jsfile.forEach((f, i) => {
         let props = require(`./commands/${f}`);
