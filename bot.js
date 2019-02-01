2ba0851414ec620a5803db5c953bfdfe6a075836
Justin2528 <38654142+Justin2528@users.noreply.github.com>
2019-02-01 19:08:50 +0800

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,5 +1,6 @@
 const Discord = require("discord.js");
 const botconfig = require("./botconfig.json");
+const fs = require("fs");
 const bot = new Discord.Client({disableEveryone: true});
 bot.commands = new Discord.Collection();
 const antispam = require("discord-anti-spam");
