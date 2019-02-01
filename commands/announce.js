19564d66b9e870dd749f1b81073c36583e2a0ab1
Justin2528 <38654142+Justin2528@users.noreply.github.com>
2019-02-01 21:59:31 +0800

    Update announce.js

--- commands/announce.js
+++ commands/announce.js
@@ -3,7 +3,7 @@ const Discord = require('discord.js');
 
 exports.run = (bot, message, args) => {
 
-     let L = bot.guild.channels.find("name", "announcements");
+     let L = message.guild.channels.find("name", "announcements");
     L.send(`@everyone ${message.content}`)
     
     
